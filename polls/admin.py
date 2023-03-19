c746225e030a05a74eb11207cdd656caa47c5138
Vladimir Kamyshanov <viator3m@yandex.ru>
2023-03-19 11:35:57 +0300

    Add polls models and admin panel for it

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from nested_admin.nested import (
+    NestedTabularInline,
+    NestedStackedInline,
+    NestedModelAdmin
+)
 
-# Register your models here.
+from polls.models import Poll, Question, Answer, UserPollResult
+
+
+class AnswerInline(NestedTabularInline):
+    model = Answer
+    fields = ('text', 'correct')
+    extra = 0
+    min_num = 1
+    max_num = 4
+
+
+class QuestionInline(NestedStackedInline):
+    model = Question
+    inlines = (AnswerInline, )
+    fields = ('text', )
+    inline_classes = ('grp-collapse grp-open',)
+    min_num = 1
+    extra = 0
+
+
+@admin.register(Poll)
+class PollAdmin(NestedModelAdmin):
+    fields = ('title', 'description', 'image')
+    list_display = ('title', 'description', 'pub_date')
+    inlines = (QuestionInline, )
+
+
+@admin.register(UserPollResult)
+class ResultAdmin(admin.ModelAdmin):
+    fields = ('user', 'poll', 'done', 'correct_answer')
+    list_display = ('user', 'poll', 'done', 'correct_answer')
+
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,91 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    title = models.CharField(
+        max_length=256,
+        verbose_name='Заголовок'
+    )
+    description = models.CharField(
+        max_length=256,
+        verbose_name='Описание'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата добавления'
+    )
+    image = models.ImageField(
+        blank=True,
+        upload_to='img',
+        verbose_name='Изображение',
+    )
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=256, verbose_name='Текст вопроса')
+    poll = models.ForeignKey(
+        Poll,
+        on_delete=models.CASCADE,
+        verbose_name='Тест'
+    )
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+    def get_answers_list(self):
+        return [(answer.id, answer.text) for answer in (
+            Answer.objects.filter(question=self))]
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=256, verbose_name='Текст')
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        verbose_name='Вопрос'
+    )
+    correct = models.BooleanField(verbose_name='Правильный')
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответ'
+
+    def __str__(self):
+        return self.text
+
+
+class UserPollResult(models.Model):
+    # avoid circle import
+    user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+    )
+    poll = models.ForeignKey(
+        Poll,
+        on_delete=models.CASCADE,
+        verbose_name='Опрос'
+    )
+    done = models.IntegerField(
+        default=0,
+        verbose_name='Всего ответов'
+    )
+    correct_answer = models.IntegerField(
+        default=0,
+        verbose_name='Правильных ответов'
+    )
+
+    class Meta:
+        verbose_name = 'Результат'
+        verbose_name_plural = 'Результаты'
+
+    def __str__(self):
+        return f'{self.user}: тест №{self.poll.id}'
